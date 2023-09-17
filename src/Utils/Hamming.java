ead30e4febd793dce89733d8ec0aa6193bda78c5
Nan Jiang <ibiza.jiang@outlook.com>
2023-09-17 17:11:05 +0800

    feat: finished

--- src/Utils/Hamming.java
+++ src/Utils/Hamming.java
@@ -1,4 +1,22 @@
 package Utils;
 
 public class Hamming {
+
+    public static double getSimilarity(String simHash1, String simHash2) {
+        int distance = getHammingDistance(simHash1, simHash2);
+        // 计算相似度
+        return 0.01 * (100 - distance * 100 / 128);
+    }
+
+    public static int getHammingDistance(String simHash1, String simHash2) {
+        int distance = 0;
+        if (simHash1.length() != simHash2.length()) {
+            distance = -1;
+        } else {
+            for (int i = 0; i < simHash1.length(); i++) {
+                if (simHash1.charAt(i) != simHash2.charAt(i)) distance++;
+            }
+        }
+        return distance;
+    }
 }
--- src/test/TestMain.java
+++ src/test/TestMain.java
@@ -1,10 +1,20 @@
 package test;
 
+import com.sun.tools.javac.Main;
 import org.junit.Test;
 
 public class TestMain {
+
     @Test
     public void easyTest(){
+        String[] stringArgs = new String[3];
+        stringArgs[0] = "/Users/jn/java/DuplicateChecking/f1.txt";
+        stringArgs[1] = "/Users/jn/java/DuplicateChecking/f2.txt";
+        stringArgs[2] = "/Users/jn/java/DuplicateChecking/f3.txt";
+        try {
+            Main.main(stringArgs);
+        } catch (Exception e) {
 
+        }
     }
 }
